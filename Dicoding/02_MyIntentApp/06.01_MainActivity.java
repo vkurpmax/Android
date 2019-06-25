042178644226be8f3850fd7366e46643dd99b1e0
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-06-25 22:52:07 +0800

    Create 06.01_MainActivity.java

---
+++ Dicoding/02_MyIntentApp/06.01_MainActivity.java
@@ -0,0 +1,9 @@
+Button btnMoveActivity;
+ 
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+ 
+    btnMoveActivity = findViewById(R.id.btn_move_activity);
+}
